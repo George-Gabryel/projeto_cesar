f8c988840fd69f5c152c996754560bdf5f046720
George-Gabryel <george.gabryel07@gmail.com>
2026-05-14 17:38:31 -0300

    origin/main

--- usuarios.py
+++ usuarios.py
@@ -1,32 +1,34 @@
-import funcoes as f
+# import funcoes as f
 
 
-perfil = {
-    "usuario" : [],
-    "email" : [],
-    "telefone" : [],
-    "senha" : []
+# perfil = {
+#     "usuario" : [],
+#     "email" : [],
+#     "telefone" : [],
+#     "senha" : []
 
-}
+# }
 
 
-email_usuario = input("Escreva um e-mail válido: ")
+# email_usuario = input("Escreva um e-mail válido: ")
 
-nome_usuario = input("Informe o nome de usuário: ")
+# nome_usuario = input("Informe o nome de usuário: ")
 
-while True:
-    try:
+# while True:
+#     try:
 
-        telefone = int(input("Informa um telefone: "))
-        break
+#         telefone = int(input("Informa um telefone: "))
+#         break
 
-    except ValueError:
-        print("Por favor, digite um número válido válida")
+#     except ValueError:
+#         print("Por favor, digite um número válido válida")
         
-senha = input("Informa uma senha: ")
+# senha = input("Informa uma senha: ")
 
-inserir_dados = f.inserindo(perfil, email_usuario, nome_usuario, telefone, senha)
-
-permissao = str(input("Informe qual o nível de permissão o usuário terá: "))
-
+# inserir_dados = f.inserindo(perfil, email_usuario, nome_usuario, telefone, senha)
 niveis_permissoes = ["Funcionário", "Cliente", "Gerente", ]
+
+permissao = str(input(f"Informe qual o nível de permissão o usuário terá{niveis_permissoes}: "))
+
+
+
