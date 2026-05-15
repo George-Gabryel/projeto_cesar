6e6481758799f972efe8637d47b5447e5c9dde12
George-Gabryel <george.gabryel07@gmail.com>
2026-05-15 12:14:27 -0300

    origin/main

--- usuarios.py
+++ usuarios.py
@@ -26,9 +26,21 @@
 # senha = input("Informa uma senha: ")
 
 # inserir_dados = f.inserindo(perfil, email_usuario, nome_usuario, telefone, senha)
-niveis_permissoes = ["Funcionário", "Cliente", "Gerente", ]
 
-permissao = str(input(f"Informe qual o nível de permissão o usuário terá{niveis_permissoes}: "))
+niveis_permissoes = ("Funcionário", "Cliente", "Gerente", )
 
+permissao = input(f"Informe qual o nível de permissão o usuário terá{niveis_permissoes}: ")
 
-
+while True:
+    if permissao == "Funcionário" or permissao == "funcionário" or permissao == "funcionario":
+        print("Cadastrado como funcionário")
+        break
+    elif permissao == "Cliente" or permissao == "cliente":
+        print("Cadastrado como cliente")
+        break
+    elif permissao == "Gerente" or permissao == "gerente":
+        print("Cadastrado como gerente")
+        continue
+else:
+    print("Coloque uma permissão válida")
+    
