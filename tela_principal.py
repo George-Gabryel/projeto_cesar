ea544645e1d3be564b360651aa018332b0914f6a
George-Gabryel <george.gabryl07@gmail.com>
2026-05-16 21:01:45 -0300

    origin/main

---
+++ tela_principal.py
@@ -0,0 +1,7 @@
+import usuarios as user
+import funcoes as fun
+
+print("Bem vindo a tela principal")
+
+escolha = int(input("O que você deseja fazer:"))
+print("1 - Cadastrar novo usuário \n 2 - Visualizar usuários criados")
--- usuarios.py
+++ usuarios.py
@@ -1,47 +1,51 @@
-# import funcoes as f
+def cadastro():
+
+    import funcoes as f
 
 
-# perfil = {
-#     "usuario" : [],
-#     "email" : [],
-#     "telefone" : [],
-#     "senha" : []
+    perfil = {
+        "usuario" : [],
+        "email" : [],
+        "telefone" : [],
+        "senha" : []
 
-# }
+    }
 
 
-# email_usuario = input("Escreva um e-mail válido: ")
+    email_usuario = input("Escreva um e-mail válido: ")
 
-# nome_usuario = input("Informe o nome de usuário: ")
+    nome_usuario = input("Informe o nome de usuário: ")
 
-# while True:
-#     try:
+    while True:
+        try:
 
-#         telefone = int(input("Informa um telefone: "))
-#         break
+            telefone = int(input("Informa um telefone: "))
+            break
 
-#     except ValueError:
-#         print("Por favor, digite um número válido válida")
-        
-# senha = input("Informa uma senha: ")
+        except ValueError:
+            print("Por favor, digite um número válido válida")
+            
+    senha = input("Informa uma senha: ")
 
-# inserir_dados = f.inserindo(perfil, email_usuario, nome_usuario, telefone, senha)
+    inserir_dados = f.inserindo(perfil, email_usuario, nome_usuario, telefone, senha)
 
-niveis_permissoes = ["Funcionário", "Cliente", "Gerente", ]
+    tipo_perfil = ["Funcionário", "Cliente", "Gerente", ]
 
-# permissao = input(f"Informe qual o nível de permissão o usuário terá{niveis_permissoes}: ")
+    while True:
+        print(f"Perfis de usuários: {tipo_perfil}")
+        permissao = input(f"Informe qual o tipo de perfil do usuário:")
+        if permissao == "Funcionário" or permissao == "funcionário" or permissao == "funcionario":
+            print("Cadastrado como funcionário")
+            break
+        elif permissao == "Cliente" or permissao == "cliente":
+            print("Cadastrado como cliente")
+            break
+        elif permissao == "Gerente" or permissao == "gerente":
+            print("Cadastrado como gerente")
+            break
+        else:
+            print("Escolha um tipo de permissção válida")
+            continue
 
-while True:
-    permissao = input(f"Informe qual o nível de permissão o usuário terá{niveis_permissoes}: ")
-    if permissao == "Funcionário" or permissao == "funcionário" or permissao == "funcionario":
-        print("Cadastrado como funcionário")
-        break
-    elif permissao == "Cliente" or permissao == "cliente":
-        print("Cadastrado como cliente")
-        break
-    elif permissao == "Gerente" or permissao == "gerente":
-        print("Cadastrado como gerente")
-        break
-    else:
-        print("Informne uma permissão válida")
-        continue
+
+    print(f"Usuário {perfil['usuario']} cadastrado com suceso")
