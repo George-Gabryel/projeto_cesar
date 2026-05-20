8c09e24accb7cc31f045f3c1d5144e0ba60d31a5
George-Gabryel <george.gabryel07@gmail.com>
2026-05-20 19:26:01 -0300

    origin/main
    the commit message for your changes. Lines starting

--- funcoes.py
+++ funcoes.py
@@ -8,17 +8,22 @@ def inserindo(padrao, a, b, c, d, e):
     padrao["tipo_perfil"].append(e)
 
 def alterar_usuario(padrao):
-    verificacao = input("Informe o e-mail do usuário que será alterado: ")
-   
+    nome_antigo = input("Qual nome deseja alterar: ")
+    nome_novo = input("Qual o novo nome: ")
+
     encontrado = False
-    for posicao_email, email_atual in enumerate(padrao["email"]):
-        if email_atual == verificacao:
-            for posicao_usuario in (padrao["usuario"]):
-                if posicao_email == posicao_usuario:
-                    nome_novo = input("Qual o novo nome: ")
-                    padrao["usuario"][posicao_usuario] = nome_novo
-                    encontrado = True
-                    break
+    for posicao, nome_atual in enumerate(padrao["usuario"]):
+        if nome_atual == nome_antigo:
+            padrao["usuario"][posicao] = nome_novo
+            encontrado = True
+            break
+
+        if encontrado:
+            print("O usuário foi alterado com sucesso!")
+            break
+        else:
+            print(f"O usuário {nome_antigo} não está cadastrado.")
+
 
         if encontrado:
             print("O usuário foi alterado com sucesso!")
