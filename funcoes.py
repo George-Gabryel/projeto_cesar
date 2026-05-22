3709d3a9d9597c6a3b457f633f21e0db8600c375
George-Gabryel <george.gabryel07@gmail.com>
2026-05-22 17:09:11 -0300

    origin/main

--- funcoes.py
+++ funcoes.py
@@ -11,12 +11,14 @@ def alterar_usuario(padrao):
     verificacao = input("Informe o e-mail do usuário que será alterado: ")
    
     encontrado = False
+    
     for posicao_email, email_atual in enumerate(padrao["email"]):
         if email_atual == verificacao:
-            for posicao_usuario in (padrao["usuario"]):
-                if posicao_email == posicao_usuario:
-                    nome_novo = input("Qual o novo nome: ")
-                    padrao["usuario"][posicao_usuario] = nome_novo
+            for posicao_usuario, usuario in enumerate(padrao["usuario"]):
+                if posicao_usuario == posicao_email:
+                    print("Nome de usuário -->", padrao["usuario"][posicao_email])
+                    usuario = input("Qual será o novo nome do usuário:  ")
+                    padrao["usuario"][posicao_email] = usuario
                     encontrado = True
                     break
 
@@ -31,20 +33,33 @@ def alterar_usuario(padrao):
 
 def alterar_email(padrao):
     email_antigo = input("Qual email sera alterado: ")
+    if validar.validar_email(email_antigo):
 
-    while True:
-        email_novo = input("Qual sera o novo email: ")
-        if validar.validar_email(email_novo):
-            break
-        print("Email invalido, tente novamente.")
 
-    for posicao, email_atual in enumerate(padrao["email"]):
-        if email_atual == email_antigo:
-            padrao["email"][posicao] = email_novo
-            print("Email alterado")
-            return
-    print(f"email {email_antigo} nao esta cadastrado. Cadastre-o ou informe um email válido")
+        while True:
+            email_encontrado = False # Variável de controle
+            
+            for posicao, email_atual in enumerate(padrao["email"]):
+                if email_atual == email_antigo:
+                    email_novo = input("Qual será o novo email: ")
+                    
+                    if validar.validar_email(email_novo):
+                        padrao["email"][posicao] = email_novo
+                        print("Email alterado")
+                        email_encontrado = True
+                        return # Para o loop 'for' pois já achou e alterou
+                    else:
+                        print("Email inválido. Tente novamente.")
+                        # Aqui você decide se quer dar break ou tentar de novo
+            
+            if email_encontrado:
+                break # Para o loop 'while True' se desejar encerrar o programa
+            else:
+                print("E-mail antigo não encontrado.")
+                break # Para evitar loop infinito caso não queira pedir o email_antigo de novo
 
+    else:
+        print("Email não reconhecido. Tente novamente e escreva um email válido")
 
 
 
--- teste.py
+++ teste.py
@@ -1,18 +1,55 @@
-verificacao = input("Informe o e-mail do usuário que será alterado: ")
-   
-encontrado = False
-
-for posicao_email, email_atual in enumerate(padrao["email"]):
-        if email_atual == verificacao:
-            for posicao_usuario in (padrao["usuario"]):
-                if posicao_email == posicao_usuario:
-                    nome_novo = input("Qual o novo nome: ")
-                    padrao["usuario"][posicao_usuario] = nome_novo
-                    encontrado = True
-                    break
+import validacao as validar
 
 padrao = {
-        "usuario" : []
+        "usuario" : ["George", "Hugo", "Fábio"],
+        "email" : ["george.gabryel07@gmail.com", "hugo.leonardo@gmail.com", "fabio.viana@gmail.com"],
+        "telefone" : [],
+        "senha" : [],
+        "tipo_perfil": []
     }
 
-padrao["usuario"[posicao_email]]
+# verificacao = input("Informe o e-mail do usuário que será alterado: ")
+   
+# encontrado = False
+
+# for posicao_email, email_atual in enumerate(padrao["email"]):
+#         if email_atual == verificacao:
+#             for posicao_usuario, usuario in enumerate(padrao["usuario"]):
+#                 if posicao_usuario == posicao_email:
+#                     print("Nome de usuário -->", padrao["usuario"][posicao_email])
+#                     usuario = input("Qual será o novo nome do usuário:  ")
+#                     padrao["usuario"][posicao_email] = usuario
+#                     encontrado = True
+#                     break
+
+
+# print(padrao["usuario"])
+email_antigo = input("Qual email sera alterado: ")
+if validar.validar_email(email_antigo):
+
+
+    while True:
+        email_encontrado = False # Variável de controle
+        
+        for posicao, email_atual in enumerate(padrao["email"]):
+            if email_atual == email_antigo:
+                email_novo = input("Qual será o novo email: ")
+                
+                if validar.validar_email(email_novo):
+                    padrao["email"][posicao] = email_novo
+                    print("Email alterado")
+                    return
+                    email_encontrado = True
+                    break  # Para o loop 'for' pois já achou e alterou
+                else:
+                    print("Email inválido. Tente novamente.")
+                    # Aqui você decide se quer dar break ou tentar de novo
+        
+        if email_encontrado:
+            break # Para o loop 'while True' se desejar encerrar o programa
+        else:
+            print("E-mail antigo não encontrado.")
+            break # Para evitar loop infinito caso não queira pedir o email_antigo de novo
+
+else:
+    print("Email não reconhecido. Tente novamente e escreva um email válido")
